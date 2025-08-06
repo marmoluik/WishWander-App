769072f47f945ff06ae788c542c706613f19340e
Marmo Luik <marmoluik@gmail.com>
2025-08-06 17:42:47 +0300

    Logo

--- components/HeaderLogo.tsx
+++ components/HeaderLogo.tsx
@@ -4,8 +4,8 @@ import { Image } from "react-native";
 export default function HeaderLogo() {
   return (
     <Image
-      source={require("@/assets/images/icon.png")}
-      style={{ width: 32, height: 32 }}
+      source={require("@/assets/images/wishwander_logo.png")}
+      style={{ width: 200, height: 50 }}
       resizeMode="contain"
     />
   );
