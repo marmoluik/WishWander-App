ab07a41bc9d708354165af6a00e0ceb078123250
Marmo Luik <marmoluik@gmail.com>
2025-08-06 21:43:45 +0300

    feat: modernize color palette

--- app/index.tsx
+++ app/index.tsx
@@ -25,7 +25,7 @@ export default function HomeScreen() {
   if (isLoading) {
     return (
       <View className="flex-1 justify-center items-center">
-        <ActivityIndicator size="large" color="#F4C430" />
+        <ActivityIndicator size="large" color="#3B82F6" />
       </View>
     );
   }
